ecbcfdc0679de987209403a8006c4aaa985b572f
benericsi <bene.richard24@gmail.com>
2023-04-15 17:28:39 +0200

    routing v1

--- MyApp/src/app/app-routing.module.ts
+++ MyApp/src/app/app-routing.module.ts
@@ -1,7 +1,36 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+  path: 'home',
+  loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule)
+  },
+  {
+  path: 'gallery',
+  loadChildren: () => import('./pages/gallery/gallery.module').then(m => m.GalleryModule)
+  },
+  {
+  path: 'item',
+  loadChildren: () => import('./pages/item/item.module').then(m => m.ItemModule)
+  },
+  {
+  path: 'favourites',
+  loadChildren: () => import('./pages/favourites/favourites.module').then(m => m.FavouritesModule)
+  },
+  {
+  path: 'contact',
+  loadChildren: () => import('./pages/contact/contact.module').then(m => m.ContactModule)
+  },
+  {
+  path: 'admin',
+  loadChildren: () => import('./pages/admin/admin.module').then(m => m.AdminModule)
+  },
+  {
+  path: 'not-found',
+  loadChildren: () => import('./pages/not-found/not-found.module').then(m => m.NotFoundModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
