509cbd75cce48cbaf39f3e35b343738e7284a14b
benericsi <bene.richard24@gmail.com>
2023-04-21 18:41:39 +0200

    signup form

--- MyApp/src/app/pages/home/home.component.ts
+++ MyApp/src/app/pages/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component} from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { User } from '../../shared/models/User';
 
 @Component({
   selector: 'app-home',
@@ -7,7 +8,30 @@ import { FormControl, FormGroup } from '@angular/forms';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent{
+  
 
+  constructor(private fb: FormBuilder) { }
+
+  signupForm = this.createForm({
+    email: '',
+    name: {
+      firstname: '',
+      lastname: ''
+    },
+    password: '',
+  })
+
+  createForm(model: User) {
+    let formGroup = this.fb.group(model);
+    formGroup.get('email')?.addValidators([Validators.required, Validators.email]);
+    formGroup.get('firstname')?.addValidators([Validators.required]);
+    formGroup.get('lastname')?.addValidators([Validators.required]);
+    formGroup.get('password')?.addValidators([Validators.required, Validators.minLength(5)]);
+    formGroup.get('rePassword')?.addValidators([Validators.required]);
+    return formGroup;
+  }
+
+  /*
   signupForm = new FormGroup({
     email: new FormControl(''),
     password: new FormControl(''),
@@ -17,8 +41,13 @@ export class HomeComponent{
       lastname: new FormControl('')
     })
   });
+  */
   
-  
+  onSubmit() {
+    if (this.signupForm.valid) {
+      console.log(this.signupForm.get('email')?.value);
+    }
+  }
   
   
 }
