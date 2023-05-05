cc74d9dd5414667e0f2b792960dbc3f491efdc0a
benericsi <bene.richard24@gmail.com>
2023-05-05 22:49:36 +0200

    a

--- MyApp/src/app/app-routing.module.ts
+++ MyApp/src/app/app-routing.module.ts
@@ -42,6 +42,10 @@ const routes: Routes = [
   loadChildren: () => import('./pages/contact/contact.module').then(m => m.ContactModule)
   },
   {
+  path: 'admin',
+  loadChildren: () => import('./pages/admin/admin.module').then(m => m.AdminModule)
+  },
+  {
   path: 'not-found',
   loadChildren: () => import('./pages/not-found/not-found.module').then(m => m.NotFoundModule)
   },
